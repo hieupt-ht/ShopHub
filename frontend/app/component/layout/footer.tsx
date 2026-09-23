8de4d2944d77ba36a4f06660b46015976bbba864
Le Khac Hieu <lekhachieupt@gmail.com>
2026-09-23 22:07:03 +0700

    render ui success

---
+++ frontend/app/component/layout/footer.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='w-full bg-[#0F172A]'>
+        <div>
+            <div className='flex justify-center'>
+                <div>
+                    logo
+                </div>
+                <h3 className='text-[#FFFFFF]'>ShopHub Việt Nam</h3>
+            </div>
+            <div>
+                <p>Giải pháp thương mại điện tử MVP tối giản và tốc độ.
+                     Đồng hành cùng các doanh nghiệp Việt kiến tạo trải nghiệm mua sắm mượt mà.</p>
+            </div>
+        </div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
---
+++ frontend/app/component/layout/header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+        <div className='w-full'>
+            <div className='bg-[#0F172A] py-[4px]'>
+                <p className='text-[FFFFFF]'>Chào mừng đến với ShoHub - Hệ thống mua sắm trực tuyến đáng tin cậy</p>
+                <div>
+                    <p className='text-[#FFFFFF]'>Hotline: 1900 6789</p>
+                    <p className='text-[#FFFFFF opacity-30'>|</p>
+                    <p className='text-[#FFFFFF]'>Trợ giúp</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
